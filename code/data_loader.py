b4ca57ab3af839aa7cc6d99cbb3ff35d4ae935ba
Urokoz <urokoz@gmail.com>
2023-11-06 23:24:25 +0100

    Added indexing to allow for random sampling from file for batches

--- code/data_loader.py
+++ code/data_loader.py
@@ -1,101 +1,104 @@
 import torch
 import os
 import sys
+import random
 import numpy as np
 from tqdm import tqdm
 from typing import List
 
 
 class StreamDataLoader:
-    def __init__(self, filename, batch_size, use_cuda=False):
+    def __init__(self, filename, batch_size, use_cuda=False, shuffle_seed=None):
         self.filename = filename
         self.batch_size = batch_size
         self.use_cuda = use_cuda and torch.cuda.is_available()
         self.device = torch.device("cuda" if self.use_cuda else "cpu")
         self.indexes = self.indexer(self.filename)
+        if shuffle_seed:
+            random.seed(shuffle_seed)
+            random.shuffle(self.indexes)
         
     
     def __iter__(self):
-        with open(self.filename, "r") as infile:
-            header = infile.readline()
+        with open(self.filename, "rb") as infile:
             batch = []
             t = tqdm(total=len(self.indexes))
-            for i, line in enumerate(infile):
-                t.update()
-                data_point = self.load_data_point(line)
+            for i in range(0, len(self.indexes), self.batch_size): 
+                batch = np.loadtxt([extract(infile, start, stop) for [start, stop] in self.indexes[i:i+self.batch_size]])
+                t.update(len(batch))
+                batch = self.process_batch(batch)
                 
-                batch.append(data_point)
-                
-                if len(batch) == self.batch_size:
-                    batch_tensor = self.process_batch(batch)
-                    
-                    if self.use_cuda:
-                        batch_tensor = batch_tensor.to(self.device)
-                
-                    yield batch_tensor
-                
-                    batch = []
-            
-            if batch:
-                batch_tensor = self.process_batch(batch)
-                    
                 if self.use_cuda:
-                    batch_tensor = batch_tensor.to(self.device)
-                
-                yield batch_tensor  
+                    batch = batch.to(self.device)
+
+                yield batch
         t.close()
                 
     
     def indexer(self, filename):
         # open file with byteread
         filesize = os.stat(filename).st_size
-        t = tqdm(total=filesize)
         try:
             infile = open(filename, "rb")
+            header = infile.readline()
         except IOError as err:
             sys.exit("Cant open file:" + str(err))
 
+        t = tqdm(total=filesize-len(header))
         # initiate flags and position in file
+        start_nums = True 
+        end_nums = False
         index_list = []
-        pos = 0
-        dp_start = 0
+        pos = len(header)
         chunk_size = 64**3
         while True:
             chunk = infile.read(chunk_size) # read chunk
             index = 0
 
             while True:     # seek through end of chunk
-                index = chunk.find(b"\n", index+1)
-                if index == -1:     # header not found
-                    break
-                else:       # datapoint end found
-                    dp_end = pos + index - 1
-                    index_list.append([dp_start, dp_end])
+                if start_nums:
+                    index = chunk.find(b"\t", index)
+                    
+                    if index == -1:
+                        break
+                    else:
+                        dp_start = pos + index +1
+                        start_nums = False 
+                        end_nums = True
 
-                    dp_start = pos + index
+                if end_nums:
+                    index = chunk.find(b"\n", index)
+                    if index == -1:     # header not found
+                        break
+                    else:       # datapoint end found
+                        dp_end = pos + index
+                        index_list.append([dp_start, dp_end])
+                        start_nums = True 
+                        end_nums = False
+
             t.update(len(chunk))
             if len(chunk) < chunk_size:
+                t.close()
                 break
 
             pos += chunk_size      # keep track of position in file
         infile.close()
-        t.close()
         return index_list
-            
-    
-    def load_data_point(self, line: str):
-        # Convert the line to a NumPy array, skipping the first entry
-        return np.fromstring(line.strip().split("\t", 1)[1], dtype=float, sep='\t')
     
     
     def process_batch(self, batch) -> torch.Tensor:
         return torch.tensor(np.array(batch), dtype=torch.float)
 
 
+def extract(buff, start, stop):
+    buff.seek(start)
+    return buff.read(stop-start)
+
+
 if __name__ == "__main__":
     filename = "data/archs4_gene_small.tsv"
     
-    dataloader = StreamDataLoader(filename, 10)
+    dataloader = StreamDataLoader(filename, 10, shuffle_seed=42)    
     
     for batch in dataloader:
         continue
