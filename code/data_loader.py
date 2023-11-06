bb1e714d374222b5f0e3cd73a44e7970001a6069
Mathias Rahbek-Borre <urokoz@gmail.com>
2023-11-06 17:59:20 +0100

    trying to optimize with profiling

--- code/data_loader.py
+++ code/data_loader.py
@@ -1,43 +1,9 @@
 import torch
 import os
 import sys
+import numpy as np
+from tqdm import tqdm
 from typing import List
-
-
-
-def indexer(filename):
-    # open file with byteread
-    filesize = os.stat(filename).st_size
-    try:
-        infile = open(filename, "rb")
-    except IOError as err:
-        sys.exit("Cant open file:" + str(err))
-
-    # initiate flags and position in file
-    index_list = []
-    pos = 0
-    dp_start = 0
-    first_flag = True
-    chunk_size = 64**3
-    while True:
-        chunk = infile.read(chunk_size) # read chunk
-        index = 0
-
-        while True:     # seek through end of chunk
-            index = chunk.find(b"\n", index+1)
-            if index == -1:     # header not found
-                break
-            else:       # datapoint end found
-                dp_end = pos + index - 1
-                index_list.append([dp_start, dp_end])
-
-                dp_start = pos + index
-
-        if len(chunk) < chunk_size:
-            break
-
-        pos += chunk_size      # keep track of position in file
-    infile.close()
 
 
 class StreamDataLoader:
@@ -46,15 +12,16 @@ class StreamDataLoader:
         self.batch_size = batch_size
         self.use_cuda = use_cuda and torch.cuda.is_available()
         self.device = torch.device("cuda" if self.use_cuda else "cpu")
-        # self.indexes = self.indexer(self.filename)
+        self.indexes = self.indexer(self.filename)
         
     
     def __iter__(self):
         with open(self.filename, "r") as infile:
             header = infile.readline()
             batch = []
-            for line in infile:
-                
+            t = tqdm(total=len(self.indexes))
+            for i, line in enumerate(infile):
+                t.update()
                 data_point = self.load_data_point(line)
                 
                 batch.append(data_point)
@@ -76,11 +43,13 @@ class StreamDataLoader:
                     batch_tensor = batch_tensor.to(self.device)
                 
                 yield batch_tensor  
+        t.close()
                 
     
     def indexer(self, filename):
         # open file with byteread
         filesize = os.stat(filename).st_size
+        t = tqdm(total=filesize)
         try:
             infile = open(filename, "rb")
         except IOError as err:
@@ -90,7 +59,6 @@ class StreamDataLoader:
         index_list = []
         pos = 0
         dp_start = 0
-        first_flag = True
         chunk_size = 64**3
         while True:
             chunk = infile.read(chunk_size) # read chunk
@@ -105,27 +73,30 @@ class StreamDataLoader:
                     index_list.append([dp_start, dp_end])
 
                     dp_start = pos + index
-
+            t.update(len(chunk))
             if len(chunk) < chunk_size:
                 break
 
             pos += chunk_size      # keep track of position in file
         infile.close()
+        t.close()
+        return index_list
             
     
-    def load_data_point(self, line: str) -> List:
-        return [float(x) for x in line.strip().split("\t")[1:]]
+    def load_data_point(self, line: str):
+        # Convert the line to a NumPy array, skipping the first entry
+        return np.fromstring(line.strip().split("\t", 1)[1], dtype=float, sep='\t')
     
     
     def process_batch(self, batch) -> torch.Tensor:
-        return torch.tensor(batch, dtype=torch.float)
+        return torch.tensor(np.array(batch), dtype=torch.float)
 
 
 if __name__ == "__main__":
-    filename = "data/archs_gene_very_small.tsv"
+    filename = "data/archs4_gene_small.tsv"
     
     dataloader = StreamDataLoader(filename, 10)
     
     for batch in dataloader:
-        print(batch)
+        continue
     
